9e1cc9676c85c6fe91df9eff7fc1fd0789f7bbf6
Ashutosh Mishra <ashutoshmishra@Ashutoshs-MacBook-Air.local>
2017-09-18 19:48:34 -0400

    Added to Twitter scraping module

--- webscraper.py
+++ webscraper.py
@@ -2,8 +2,21 @@ import bs4 as bs
 import urllib.request
 
 # Returns a numpy array with last x (amount tbd) tweets from specified handle
-def twitterScraper(twitterHandles):
-	# TODO figure out if I should use BS4, Twitter API, or Scrapy
+def twitterScraper(twitterHandles, tweetLimit=5):
 	for handle in twitterHandles:
 		url = "https://twitter.com/" + str(handle)
-		pageHTML = urllib.request.urlopen(url).read()
+		pageHTML = urllib.request.urlopen(url)
+
+		soup = bs.BeautifulSoup(pageHTML, "html.parser")
+
+		print("@"+str(handle) + "'s timeline:")
+		print("===============================")
+		twitterTimeline = soup.findAll("p", {"class":"TweetTextSize TweetTextSize--normal js-tweet-text tweet-text"})
+
+		numTweets = 0
+		for tweet in twitterTimeline:
+			numTweets+=1
+			print(str(numTweets) + ". " + tweet.text)
+
+			if numTweets==tweetLimit:
+				break
