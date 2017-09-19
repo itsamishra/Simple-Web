5d2c7d4a2315c92a89de407078bc37d5a6295f8a
Ashutosh Mishra <ashutoshmishra@Ashutoshs-MacBook-Air.local>
2017-09-18 20:40:57 -0400

    Finished Twitter module

--- main.py
+++ main.py
@@ -2,20 +2,45 @@ import pandas as pd
 import numpy as np
 import webscraper
 
+# Number of tweets from each handle allowed
+tweetLimit = 5
+
 # Gets Twitter data and converts it to text
 def getTweets(twitterContent):
-	twitterHandles = np.array(twitterContent.Content)
-	webscraper.twitterScraper(twitterHandles)
-	
+	twitterHandles = twitterContent.Content
+	tweets = np.array(webscraper.twitterScraper(twitterHandles, tweetLimit))
+
+	return tweets
+
+def writeTweets(file, tweets, handles):
+	tweetsPerHandle = int(len(tweets)/len(handles))
+
+	file.write("TWEETS\n")
+	for i in range(len(tweets)):
+		if(i%tweetsPerHandle==0):
+			file.write("@" + handles[i/tweetsPerHandle] + "\n")
+		file.write("-" + tweets[i] + "\n")
+
 
 def main():
+	# File in which everything will be written
+	outputFile = open("output.txt", "w")
+
 	# Gets content to be scraped
 	content = pd.read_csv("content.csv")
 
 	# Passes twitter data
 	twitterContent = content[content.Name=="Twitter"]
-	getTweets(twitterContent)
+	tweets = getTweets(twitterContent)
 
+	'''=====FOR=====DEBUGGING=====
+	for i in range(len(tweets)):
+		print(str(i+1) + ". " + tweets[i])
+	'''
+
+	writeTweets(outputFile, tweets, twitterContent.Content)
+
+	outputFile.close()
 
 if __name__ == '__main__':
 	main()
--- webscraper.py
+++ webscraper.py
@@ -1,22 +1,33 @@
 import bs4 as bs
 import urllib.request
 
-# Returns a numpy array with last x (amount tbd) tweets from specified handle
+# Returns an array with last x (amount tbd) tweets from specified handle
 def twitterScraper(twitterHandles, tweetLimit=5):
+	tweets = []
+
 	for handle in twitterHandles:
 		url = "https://twitter.com/" + str(handle)
 		pageHTML = urllib.request.urlopen(url)
 
 		soup = bs.BeautifulSoup(pageHTML, "html.parser")
 
+		'''=====FOR=====DEBUGGING=====
 		print("@"+str(handle) + "'s timeline:")
 		print("===============================")
+		'''
 		twitterTimeline = soup.findAll("p", {"class":"TweetTextSize TweetTextSize--normal js-tweet-text tweet-text"})
 
 		numTweets = 0
 		for tweet in twitterTimeline:
 			numTweets+=1
+
+			'''=====FOR=====DEBUGGING=====
 			print(str(numTweets) + ". " + tweet.text)
+			'''
+
+			tweets.append(tweet.text)
 
 			if numTweets==tweetLimit:
 				break
+
+	return tweets
