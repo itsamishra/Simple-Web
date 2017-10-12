995cc3428fde212e6e79097409f0a7fb1a70a45a
Ashutosh Mishra <ashutoshmishra413@gmail.com>
2017-10-12 18:09:31 -0400

    Output from modules now appear in seperate .txt files

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import webscraper
+import sys
 
 # Number of tweets from each handle allowed
 tweetLimit = 5
@@ -37,22 +38,30 @@ def writeHeadlines(file, headlines):
 		file.write(str(i+1) + " " + headlines[i][0] + " (" + headlines[i][1] + ")" + "\n")
 
 def main():
-	# File in which everything will be written
-	outputFile = open("output.txt", "w")
-
 	# Gets content to be scraped
 	content = pd.read_csv("content.csv")
 
-	# Gets tweets and writes them to .txt file
-	twitterContent = content[content.Name=="Twitter"]
-	tweets = getTweets(twitterContent)
-	writeTweets(outputFile, tweets, twitterContent.Content)
+	# Gets twitter content if requested in command line arugment
+	if "twitter" in sys.argv:
+		twitterOutput = open("./Content/twitterOutput.txt", "w")
 
-	# Gets Hacker News headlines and writes it to .txt file
-	headlines = getHN()
-	writeHeadlines(outputFile, headlines)
+		# Gets tweets and writes them to .txt file
+		twitterContent = content[content.Name=="Twitter"]
+		tweets = getTweets(twitterContent)
+		writeTweets(twitterOutput, tweets, twitterContent.Content)
 
-	outputFile.close()
+		twitterOutput.close()
+
+	# Gets Hacker News content if requested in command line arugment
+	if "hackerNews" in sys.argv:
+		hackerNewsOutput = open("./Content/hackerNewsOutput.txt", "w")
+
+		# Gets Hacker News headlines and writes it to .txt file
+		headlines = getHN()
+		writeHeadlines(hackerNewsOutput, headlines)
+
+		hackerNewsOutput.close()
+
 
 if __name__ == '__main__':
 	main()
